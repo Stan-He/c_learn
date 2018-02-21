6bb27d539de0574c613b165ca9f5d6881ac2c1ac
menghe <mengh.1988@gmail.com>
2018-02-21 14:20:58 +0800

    add ldd3 chapter2 expample code

---
+++ linux_device_driver3/chapter2/hello.c
@@ -0,0 +1,29 @@
+#include<linux/init.h>
+#include<linux/module.h>
+
+static int howmany =1;
+static char *whom = "world";
+module_param(howmany, int ,S_IRUGO);
+module_param(whom, charp, S_IRUGO);
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int hello_init(void){
+    int i =0;
+    for(i=0;i<howmany;i++){
+        printk(KERN_ALERT "Hello, %s\n",whom);
+    }
+    return 0;
+
+    }
+
+static void hello_exit(void){
+    
+    printk(KERN_ALERT "Goodbye, curel world\n");
+    
+    }
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+
