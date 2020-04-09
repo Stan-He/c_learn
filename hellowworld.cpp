373c1d4cd0926f606f6a5052b98541bc4703bc4f
meng.he <meng.he@starblaze-tech.com>
2020-04-09 03:52:52 -0400

    complete lesson2 code

--- hellowworld.cpp
+++ hellowworld.cpp
@@ -14,6 +14,19 @@ void foo(int n){
 }
 
 
+void c_plus_lesson2(){
+
+    smart_ptr<circle> ptr1(new circle());
+    printf("use count of ptr1 is %ld\n",ptr1.use_count());
+    smart_ptr<shape> ptr2;
+    printf("use count of ptr2 is %ld\n",ptr2.use_count());
+    ptr2=ptr1;
+    printf("use count of ptr2 is %ld\n",ptr2.use_count());
+    if (ptr1){
+        puts("ptr1 is not empty");
+    }
+}
+
 int main(){
 
     double array[10]={1,1,1,1,1,1,1,1,1};
@@ -29,8 +42,7 @@ int main(){
         puts(s);
     }
 
-    smart_ptr<shape> ptr1(new circle());
-    printf("use count of ptr1 is %ld\n",ptr1.use_count());
+    c_plus_lesson2();
 
 
     return 0;
